72f566a97f3003d264475193d761496bf8ae913f
Encoder-SS <75261080+Encoder-SS@users.noreply.github.com>
2020-12-15 18:14:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function getMessage(){
+    var message = document.getElementById("entry").value;
+    document.getElementById("message").innerHTML = message;
+    document.getElementById("entry").value ="";
+    document.getElementById("message").style.display="block";
+}
